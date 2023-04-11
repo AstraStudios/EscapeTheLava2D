04a4a96680683b21fb91740a4374efbc0287f7e8
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-04-10 23:09:34 -0500

    basic lava and player movement smoothing

---
+++ Assets/Scripts/GrowLava.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowLava : MonoBehaviour
+{
+    // have this be initial amount; definitely better ways to do this
+    float scaleMultiple = .001f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newScale = transform.localScale;
+        newScale.y = scaleMultiple;
+        transform.localScale = newScale;
+        if (transform.localScale.y < 4f)
+        {
+            scaleMultiple += .001f;
+        }
+        else {
+            scaleMultiple += .01f;
+        }
+    }
+}
